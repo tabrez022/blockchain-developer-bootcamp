eebc3b119609e37bfe8a7a443413959162bd6c43
tabrez022 <talam1074@gmail.com>
2024-02-02 23:45:51 +0530

    smoke test but ethers.signers() not working

---
+++ contracts/Token.sol
@@ -0,0 +1,7 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.9;
+import "hardhat/console.sol";
+
+contract Token {
+    string public name = "My Token";
+}
--- scripts/1_deploy.js
+++ scripts/1_deploy.js
@@ -4,25 +4,14 @@
 // You can also run a script with `npx hardhat run <script>`. If you do that, Hardhat
 // will compile your contracts, add the Hardhat Runtime Environment's members to the
 // global scope, and execute the script.
-const hre = require("hardhat");
 
 async function main() {
-	const currentTimestampInSeconds = Math.round(Date.now() / 1000);
-	const unlockTime = currentTimestampInSeconds + 60;
-
-	const lockedAmount = hre.ethers.parseEther("0.001");
-
-	const lock = await hre.ethers.deployContract("Lock", [unlockTime], {
-		value: lockedAmount,
-	});
-
-	await lock.waitForDeployment();
-
-	console.log(
-		`Lock with ${ethers.formatEther(
-			lockedAmount
-		)}ETH and unlock timestamp ${unlockTime} deployed to ${lock.target}`
-	);
+	// fetch the contract
+	const Token = await ethers.getContractFactory("Token");
+	//deploy the contract
+	const token = await Token.deploy();
+	await token.deployed();
+	console.log(`Token deployed at : ${token.address}`);
 }
 
 // We recommend this pattern to be able to use async/await everywhere
